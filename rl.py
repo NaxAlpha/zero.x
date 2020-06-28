9194cfb25b0751938479aff51d6c6b8238cf333e
Nauman Mustafa <nauman.mustafa.x@gmail.com>
2020-06-28 11:58:18 +0500

    implement ddpg

--- rl.py
+++ rl.py
@@ -1,58 +1,116 @@
+# https://towardsdatascience.com/deep-deterministic-policy-gradients-explained-2d94655a9b7b
+import copy
 import math
 import torch
 import random
+import numpy as np
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
 from itertools import count
-from collections import namedtuple
+from collections import deque, namedtuple
 
 
-Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
-Epsilon = namedtuple('Epsilon', 
-                        ('start', 'end', 'decay'))
-
-class ReplayMemory(object):
-
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.memory = []
-        self.position = 0
-
-    def push(self, *args):
-        """Saves a transition."""
-        if len(self.memory) < self.capacity:
-            self.memory.append(None)
-        self.memory[self.position] = Transition(*args)
-        self.position = (self.position + 1) % self.capacity
-
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
+Epsilon = namedtuple('Epsilon', ('start', 'end', 'decay'))
 
 
-class DQN(nn.Module):
+class Trigno(nn.Module):
 
-    def __init__(self, num_stats, num_controls):
-        super(DQN, self).__init__()
-        layers = [32, 64, 128, 64, 32]
-        self.model = nn.Sequential(
-            nn.Linear(num_stats, layers[0]),
-            nn.ReLU(),
-            *(nn.Sequential(
-                nn.Linear(l1, l2),
-                nn.ReLU(),
-            ) for l1, l2 in zip(layers, layers[1:])),
-            nn.Linear(layers[-1], num_controls),
-        )
+    def __init__(self):
+        super().__init__()
 
     def forward(self, x):
+        x = torch.cat([x, torch.sin(x), torch.cos(x)], -1)
+        return x
+
+
+class Outer(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        b, f = x.shape
+        y = torch.einsum('bi,bj->bij', x, x)
+        y = y.view(b, f*f)
+        y = torch.cat([x, y], -1)
+        return y
+
+
+class GenericFCN(nn.Module):
+
+    def __init__(self, n_in, n_out, layers=None):
+        super().__init__()
+        layers = layers or [256, 128, 64, 32]
+        self.model = nn.Sequential(
+            Trigno(),
+            Outer(),
+            nn.Linear(9*n_in*n_in+3*n_in, layers[0]),
+            nn.BatchNorm1d(layers[0]),
+            nn.GELU(),
+            *(nn.Sequential(
+                nn.Linear(l1, l2),
+                nn.BatchNorm1d(l2),
+                nn.GELU(),
+            ) for l1, l2 in zip(layers, layers[1:])),
+            nn.Linear(layers[-1], n_out),
+            nn.Sigmoid()
+        )
+
+    def forward(self, *x):
+        x = torch.cat(x, -1)
         return self.model(x)
     
+
+class EMANetwork:
+
+    def __init__(self, network, lr=1e-4):
+        self.network = network.train()
+        self.target = copy.deepcopy(network).eval()
+        self.optim = torch.optim.Adam(self.network.parameters(), lr)
+
+    def optimize(self, loss):
+        self.optim.zero_grad()
+        loss.backward()
+        self.optim.step()
+        self.ema()
+
+    def ema(self, mu=0.01):
+        for target_param, param in zip(self.network.parameters(), self.target.parameters()):
+            target_param.data.copy_(param.data * mu + target_param.data * (1.0 - mu))
+
+
+class Memory:
+    def __init__(self, max_size):
+        self.buffer = deque(maxlen=max_size)
+    
+    def push(self, state, action, reward, next_state, done):
+        experience = (state, action, reward.unsqueeze(0), next_state, torch.tensor([done]))
+        self.buffer.append(experience)
+
+    def sample(self, batch_size):
+        state_batch = []
+        action_batch = []
+        reward_batch = []
+        next_state_batch = []
+        done_batch = []
+
+        batch = random.sample(self.buffer, batch_size)
+
+        for experience in batch:
+            state, action, reward, next_state, done = experience
+            state_batch.append(state)
+            action_batch.append(action)
+            reward_batch.append(reward)
+            next_state_batch.append(next_state)
+            done_batch.append(done)
+        
+        return state_batch, action_batch, reward_batch, next_state_batch, done_batch
+
+    def __len__(self):
+        return len(self.buffer)
+
 
 class RLAI:
 
@@ -61,22 +119,24 @@ class RLAI:
         self.num_state = 24
         self.num_action = 8
 
-        self.replay = ReplayMemory(10000)
-        self.policy = DQN(self.num_state, self.num_action).to(self.device)
-        self.target = DQN(self.num_state, self.num_action).to(self.device)
-        self.target.load_state_dict(self.policy.state_dict())
-        self.target.eval()
+        self.replay = Memory(100000)
+        self.actor = GenericFCN(self.num_state, self.num_action)
+        self.critic = GenericFCN(self.num_state + self.num_action, self.num_action)
         
-        self.optim = optim.RMSprop(self.policy.parameters())
-        self.eps = Epsilon(0.9, 0.05, 200)
-        self.batch_size = 256
-        self.gamma = 0.999
+        self.actor = EMANetwork(self.actor.to(self.device))
+        self.critic = EMANetwork(self.critic.to(self.device))
+        
+        self.loss_fn = nn.MSELoss()
+        self.eps = Epsilon(0.9, 0.05, 10000)
+        self.batch_size = 1024
+        self.gamma = 0.99
         self.steps = 0
         self.loss = 0
 
     @torch.no_grad()
     def _sample_action(self, state):
-        action = self.policy(state)[0]
+        self.actor.network.eval()
+        action = self.actor.network(state)[0]
         self.action = action.unsqueeze(0)
         return self.action
 
@@ -98,36 +158,21 @@ class RLAI:
     def optimize_step(self):
         if len(self.replay) < self.batch_size:
             return
-
-        batch = self.replay.sample(self.batch_size)
-        batch = Transition(*zip(*batch))
-
-        non_final_mask = map(lambda s: s is not None, batch.next_state)
-        non_final_mask = torch.tensor(tuple(non_final_mask)).bool().to(self.device)
-        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
-
-        state_batch = torch.cat(batch.state)
-        action_batch = torch.cat(batch.action)
-        reward_batch = torch.cat(batch.reward)
-
-        # RE: Understand what happens below 
-        state_action_values = self.policy(state_batch).gather(1, (action_batch>0.5).long())
-        print(state_action_values.shape)
-        next_state_values = torch.zeros(self.batch_size).to(self.device)
-        print(next_state_values.shape, non_final_mask.shape, non_final_next_states.shape)
-        next_state_values[non_final_mask] = self.target(non_final_next_states)[0].detach()
         
-        expected_state_action_values = (next_state_values * self.gamma) + reward_batch
-        # End RE:
+        self.actor.network.train()
+        states, actions, rewards, next_states, _ = \
+            map(torch.cat, self.replay.sample(self.batch_size))
 
-        loss = F.binary_cross_entropy(state_action_values, expected_state_action_values.unsqueeze(1))
-        self.optim.zero_grad()
-        loss.backward()
-        for param in self.policy.parameters():
-            param.grad.data.clamp_(-1, 1)
-        self.optim.step()
+        Qvals = self.critic.network(states, actions)
+        next_actions = self.actor.target(next_states)
+        next_Q = self.critic.target(next_states, next_actions.detach())
+        Qprime = rewards + self.gamma * next_Q
 
-        self.loss = loss.item()
+        critic_loss = self.loss_fn(Qvals, Qprime)
+        policy_loss = -self.critic.network(states, self.actor.network(states)).mean()
+
+        self.actor.optimize(policy_loss)
+        self.critic.optimize(critic_loss)
 
     def init_run(self, state):
         state = state.to(self.device)
@@ -138,12 +183,12 @@ class RLAI:
         state = state.to(self.device)
         reward = torch.tensor([reward]).to(self.device)
 
-        if done:
-            state = None
+        if not done:
+            self.replay.push(self.last_state, self.action, reward, state, done)
+            self.optimize_step()
+            self.last_state = state
 
-        self.replay.push(self.last_state, self.action, state, reward)
-        self.optimize_step()
-
-        self.last_state = state
         return self.next_action(state)
         
+    def save(self, fn):
+        torch.save(self.actor.network.state_dict(), fn)
